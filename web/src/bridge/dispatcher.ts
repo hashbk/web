43fefdd047100babf970015c36738154dc5596eb
hashbk <littlefishyu2008@gmail.com>
2026-08-30 18:40:43 +0800

    style

--- web/src/bridge/dispatcher.ts
+++ web/src/bridge/dispatcher.ts
@@ -866,7 +866,6 @@ export class BridgeDispatcher {
       case "option:flutter:local": {
         const args = JSON.parse(value) as { name?: string; value?: string };
         if (args.name) setLocalOption(`flutter:${args.name}`, args.value ?? "");
-
         return "";
       }
       case "option:flutter:peer": {
